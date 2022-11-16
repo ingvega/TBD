4349b4579a7575080368f3d9162b41601f3b5254
ingvega <isc.p.vega@gmail.com>
2022-11-16 11:21:28 -0600

    Integración inicial del proyecto

---
+++ TBD/src/datos/DAOProducto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+import modelos.Producto;
+/**
+ *DataAccessObject
+ * @author paveg
+ */
+public class DAOProducto {
+    //Operaciones
+    public int agregar(Producto objProducto){
+    
+    }
+    public int editar(Producto objProducto){
+    eliminar
+    consultarTodos
+    consultarActivos
+    consultarUno
+        
+}
